b36932bf419c7bf761477550af9b9b4507d7d022
DUONG TIEN HIEU <tienhieuD@users.noreply.github.com>
2020-12-10 11:53:35 +0700

    Update category

--- listview_sticky_header_and_column/__manifest__.py
+++ listview_sticky_header_and_column/__manifest__.py
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Technical Settings',
+    'category': 'Tools',
     'version': '0.2',
     # any module necessary for this one to work correctly
     'depends': ['base', 'web'],
